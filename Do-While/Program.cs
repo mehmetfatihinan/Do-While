53d63a225e3b44a33c00b4200f443700015bc141
mehmetfatihinan <mehmetfatihinan@gmail.com>
2024-12-28 19:59:44 +0300

    Add While Part and Comment

--- Do-While/Program.cs
+++ Do-While/Program.cs
@@ -7,12 +7,33 @@ namespace DoWhileAssignment
     {
         static void Main(string[] args)
         {
-            int i = 0;
+
+            // Do-While Part
+            int counter = 0;
+            int user_limit;
+            Console.WriteLine("Lütfen bir limit giriniz: ");
+            user_limit = Convert.ToInt32(Console.ReadLine());
             do
             {
-                Console.WriteLine("The value of i is: {0}", i);
-                i++;
-            } while (i < 5);
+                Console.WriteLine("Ben bir Patika'lıyım");
+                counter++;
+            } while (counter <= user_limit);
+
+
+            // While Part
+
+            counter = 0;
+            Console.WriteLine("Lütfen bir limti giriniz: ");
+            user_limit = Convert.ToInt32(Console.ReadLine());
+            while (counter <= user_limit)
+            {
+                Console.WriteLine("Ben bir Patika'lıyım");
+                counter++;
+            }
+
+            //Do-While Bölümü önce bir kere çalışır ve ardından koşul sağlanırsa çalışmaya devam eder. Fakat While bölümü koşul sağlanırsa çalışmaya başlar. Bu nedenle
+            //Do-While bölümü bir kere çalışırken While bölümü çalışmaya başlamaz.
+
         }
     }
 }
