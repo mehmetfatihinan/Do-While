aae721639c1f139449ef2fe5309fcb738574794d
mehmetfatihinan <mehmetfatihinan@gmail.com>
2024-12-28 14:03:39 +0300

    Add project files.

---
+++ Do-While/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DoWhileAssignment
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine("The value of i is: {0}", i);
+                i++;
+            } while (i < 5);
+        }
+    }
+}
